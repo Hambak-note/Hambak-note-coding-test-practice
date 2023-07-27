8c2a189bbdb7fe93dc8c04e33090744122e89176
Younghwan <dudghks5722@gmail.com>
2023-07-27 18:42:04 +0900

    [level 2] Title: 주식가격, Time: 57.66 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+        public int[] solution(int[] prices) {
+            int[] answer = new int[prices.length];
+
+            Stack<Integer> secondStack = new Stack<>();
+
+            secondStack.push(0);
+            for(int i=1; i < prices.length; i++){
+
+                while(!secondStack.isEmpty() && prices[i] < prices[secondStack.peek()]){
+                    int idx = secondStack.pop();
+                    answer[idx] = i - idx;
+                }
+                secondStack.push(i);
+            }
+
+            while(!secondStack.isEmpty()){
+                int idx = secondStack.pop();
+                answer[idx] = prices.length-idx-1;
+            }
+
+            return answer;
+        }
+    }
