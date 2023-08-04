403cef04bf5758098a3f334708a4f70323d5bf5f
Younghwan <dudghks5722@gmail.com>
2023-08-05 03:12:50 +0900

    [level 2] Title: 의상, Time: 1.48 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -1,44 +1,24 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*; 
 
-public class Solution {
+class Solution {
+        public int solution(String[][] clothes) {
+            int answer = 0;
 
-    public int solution(String[][] clothes) {
+            HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+            for(String[] str : clothes){
+                String category = str[1];
+                map.put(category, map.getOrDefault(category, 0)+1);
+            }
 
-        //부위별로 의상의 개수를 저장하는 맵
-        Map<String, Integer> clothesCountMap = countClothesByPart(clothes);
+            Iterator<Integer> it = map.values().iterator();
+            answer = 1;
 
-        int answer = getNumberOfPossibilities(clothesCountMap);
+            while(it.hasNext())
+                answer *= it.next().intValue() +1;
 
-        return answer;
+
+           return answer-1;
+
+        }
     }
-
-    private Map<String, Integer> countClothesByPart(String[][] clothes) {
-
-        Map<String, Integer> clothesCountMap = new HashMap<>();
-
-        Arrays.stream(clothes)
-                .forEach(cloth ->
-                        clothesCountMap.put(cloth[1], clothesCountMap.getOrDefault(cloth[1], 0)+1));
-
-        return clothesCountMap;
-    }
-
-    private int getNumberOfPossibilities(Map<String, Integer> map) {
-
-        int answer =
-                map.values().stream()
-                        //부위별 의상에 1개씩 더한다.
-                        .mapToInt(count -> count + 1)
-                        //초기값을 1로 설정하여 스트림의 요소들을 모두 곱한다.
-                        .reduce(1, (a, b) -> a * b);
-
-        //모두 입지 않은 경우 1가지를 빼준다.
-        answer = answer-1;
-
-        return answer;
-    }
-}
