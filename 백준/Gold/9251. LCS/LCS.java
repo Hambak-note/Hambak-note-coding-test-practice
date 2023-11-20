088baa37ba37158db0dac4fa850ddc332aae42c5
Younghwan <dudghks5722@gmail.com>
2023-11-20 15:16:08 +0900

    [Gold V] Title: LCS, Time: 172 ms, Memory: 18808 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String first = br.readLine();
+        String second = br.readLine();
+
+        int firstLen = first.length();
+        int secondLen = second.length();
+
+        int[][] dp = new int[firstLen+1][secondLen+1];
+
+        for(int i = 1; i <= firstLen; i++) {
+            for(int j = 1; j <= secondLen; j++) {
+
+                if(first.charAt(i-1) == second.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        System.out.println(dp[firstLen][secondLen]);
+
+    }
+}
