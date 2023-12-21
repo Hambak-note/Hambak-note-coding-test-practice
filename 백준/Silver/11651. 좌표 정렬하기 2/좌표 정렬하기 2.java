aa5e96ec3fa3a98655e7b410ac07b87552ca7801
Younghwan <dudghks5722@gmail.com>
2023-12-21 18:08:02 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1628 ms, Memory: 57680 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int inputArr[][] = new int[n][2];
+
+        StringTokenizer st;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            inputArr[i][0] = Integer.parseInt(st.nextToken());
+            inputArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(inputArr, Comparator.comparingInt((int[] a) -> a[1]).thenComparingInt(a -> a[0]));
+
+
+        for (int[] ints : inputArr) {
+            System.out.println(ints[0] + " " + ints[1]);
+        }
+    }
+}
