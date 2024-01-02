d07d156d074bf83eba0504ae8c3ebb216b6372a7
Younghwan <dudghks5722@gmail.com>
2024-01-02 15:59:22 +0900

    Time: 33 ms (14.60%), Space: 45.2 MB (10.65%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean isPalindrome(String s) {
         
         String extractS = extractCharacterAndMakeLowerCase(s);
-        String reverseS = reverseString(extractS);
+        Deque<Character> deque = makeDeque(extractS);
 
-        return isPalindrome(extractS, reverseS);
+        return isPalindrome(deque);
     }
 
     private String extractCharacterAndMakeLowerCase(String s) {
@@ -13,14 +13,28 @@ class Solution {
                 .mapToObj(c -> String.valueOf((char) c).toLowerCase())
                 .collect(Collectors.joining());
     }
+    
+    private Deque<Character> makeDeque(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
 
-    private String reverseString(String s) {
-        StringBuffer sb = new StringBuffer(s);
-        return sb.reverse().toString();
+        for(int i = 0; i < s.length(); i++) {
+            deque.add(Character.toLowerCase(s.charAt(i)));
+        }
+
+        return deque;
     }
 
-    private boolean isPalindrome(String s, String reverseS) {
-        return s.equals(reverseS);
+    private boolean isPalindrome(Deque<Character> deque) {
+        while(deque.size() > 1) {
+
+            if(deque.pollFirst() != deque.pollLast())
+            {
+                System.out.println();
+                return false;
+            }
+        }
+
+        return true;
     }
 
     
