b7f4e8510d587753859626ddef934bdd1664f2cf
Younghwan <dudghks5722@gmail.com>
2024-01-04 14:20:52 +0900

    Time: 902 ms (94.65%), Space: 31.1 MB (58.46%) - LeetHub

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,13 +3,13 @@ from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         index_stack = []
-        result = [0] * len(temperatures)
+        output = [0] * len(temperatures)
 
         for i in range(len(temperatures)):
             while index_stack and temperatures[index_stack[-1]] < temperatures[i]:
                 popped_index = index_stack.pop()
-                result[popped_index] = i - popped_index
+                output[popped_index] = i - popped_index
 
             index_stack.append(i)
 
-        return result
+        return output
