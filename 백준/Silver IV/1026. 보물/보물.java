424e7909776787e7cebb0917d7547e75855ba70c
Younghwan <dudghks5722@gmail.com>
2024-02-14 10:15:23 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver IV/1026. 보물/보물.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n;
+    private static int answer = 0;
+    private static Integer[] a;
+    private static Integer[] b;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        a = new Integer[n];
+        b = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder());
+        
+        for(int i = 0; i < n; i++) {
+            answer += (a[i] * b[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+}
