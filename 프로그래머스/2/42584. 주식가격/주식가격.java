0884af5719a9941f726df7515e237f2b4255bf92
Younghwan <dudghks5722@gmail.com>
2024-03-09 18:13:13 +0900

    [level 2] Title: 주식가격, Time: 21.80 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,46 +1,28 @@
 import java.util.Stack;
 
 class Solution {
-
-        private static final int LAST = 0;
         public int[] solution(int[] prices) {
+            int[] answer = new int[prices.length];
 
-            int[] times = new int[prices.length];
-            Stack<Integer> reverseStack = new Stack<>();
 
-            for (int price : prices) {
-                reverseStack.push(price);
-            }
+            int len = prices.length;
+            Stack<Integer> stack = new Stack<>();
+            stack.add(0);
+            for(int i = 1; i < len; i++) {
 
-            times[prices.length-1] = 0;
-            reverseStack.pop();
-            while(!reverseStack.isEmpty()) {
+                while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
 
-                int curTime = reverseStack.size()-1;
-                int curPrice = reverseStack.pop();
-
-                boolean isBreak = false;
-                for (int nextTime = curTime + 1; nextTime < prices.length; nextTime++) {
-
-                    int nextPrice = prices[nextTime];
-                    if(curPrice > nextPrice) {
-                        times[curTime] = nextTime-curTime;
-                        isBreak = true;
-                        break;
-                    }
-
-                    if(curPrice == nextPrice) {
-                        times[curTime] = nextTime - curTime + times[nextTime];
-                        isBreak = true;
-                        break;
-                    }
+                    int j = stack.pop();
+                    answer[j] = i - j;
                 }
 
-                if(!isBreak) {
-                    times[curTime] = prices.length-curTime-1;
-                }
+                stack.add(i);
             }
-
-            return times;
+            
+            while(!stack.isEmpty()) {
+                int j = stack.pop();
+                answer[j] = len-1-j;
+            }
+            return answer;
         }
     }
