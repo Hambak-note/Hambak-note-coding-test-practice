22339207ad154a38f42218b9c6a5199e047434ba
Younghwan <dudghks5722@gmail.com>
2024-06-15 11:16:50 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input + "??!");
+    }
+}
