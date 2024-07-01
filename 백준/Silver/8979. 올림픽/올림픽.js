8b6c90406b735c108a41fc162a6009d96eb74402
Younghwan <dudghks5722@gmail.com>
2024-07-01 18:43:16 +0900

    [Silver V] Title: 올림픽, Time: 152 ms, Memory: 10112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.js
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const filePath = "dev/stdin";
+const filePath = "/dev/stdin";
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [n, k] = input.shift().split(" ").map(Number);
@@ -10,23 +10,25 @@ for(let i = 0; i < n; i++) {
 }
 
 countries.sort((a, b) => {
-    if(a[1] === b[1] && a[2] === b[2]) return b[3] - b[3];
+    if(a[1] === b[1] && a[2] === b[2]) return b[3] - a[3];
     if(a[1] === b[1]) return b[2] - a[2];
     return b[1] - a[1];
 });
 
 let prev = [];
-let rank = 0;
-for (const country of countries) {
+let rank = 1;
+for(let i = 0; i < countries.length; i++) {
 
-    let cur = country.slice(1);
-    if(!arraysEqual(cur, prev)) rank++;
+    let cur = countries[i].slice(1);
+    if(!arraysEqual(cur, prev)) rank = i+1;
+
     prev = cur;
-    if(country[0] === k) break;
+    if(countries[i][0] === k) break;
 }
 
-console.log(rank);
 
+if(rank === 0) rank = 1;
+console.log(rank);
 
 function arraysEqual(arr1, arr2) {
     return arr1.every((value, index) => value === arr2[index]);
