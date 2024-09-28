91fc3486060d311b468f8dffd853ca827eafd19b
Younghwan Hwang <dudghks5722@gmail.com>
2024-09-28 11:34:07 +0900

    [Silver I] Title: 카잉 달력, Time: 228 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        while(tc-- > 0) {
+
+            st = new StringTokenizer(br.readLine());
+            boolean check = false;
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            
+            for(int i = x; i < (n*m); i += m) {
+                if(i % n == y) {
+                    System.out.println(i + 1);
+                    check = true;
+                    break;
+                }
+            }
+            
+            if(!check) {
+                System.out.println(-1);
+            }
+        }
+    }
+}
