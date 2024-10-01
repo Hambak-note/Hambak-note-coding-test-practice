e25ea2a9c273c540712a1b445d82a49d61c2288c
Younghwan Hwang <dudghks5722@gmail.com>
2024-10-01 23:18:01 +0900

    [level 2] Title: [1차] 캐시, Time: 34.84 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+
+        ArrayList<String> cache = new ArrayList<>();
+
+        if(cacheSize == 0) return cities.length * 5;
+
+        for(int i = 0; i < cities.length; i++) {
+
+            String city = cities[i].toLowerCase();
+
+            if(cache.contains(city)) {
+                cache.remove(city);
+                cache.add(city);
+                answer += 1;
+                continue;
+            }
+
+            if(cache.size() == cacheSize) cache.remove(0);
+            cache.add(city);
+            answer += 5;
+        }
+
+        return answer;
+    }
+}
