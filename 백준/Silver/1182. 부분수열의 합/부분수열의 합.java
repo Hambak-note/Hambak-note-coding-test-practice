ce35d9f7989bef1fa64059b6453155a835f9f4ef
Younghwan Hwang <dudghks5722@gmail.com>
2024-10-12 21:26:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,41 +1,39 @@
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    private static int N;
-    private static int S;
-    private static int result = 0;
-    private static int[] arr = new int[30];
+    private static int n, s;
+    private static int cnt;
+    private static int[] arr;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
+
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        cnt = 0;
+        arr = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        search(0, 0);
-        if(S == 0) result = result - 1;
-        System.out.println(result);
+        for(int i = 0; i < n; i++) arr[i] =  Integer.parseInt(st.nextToken());
 
+        func(0, 0);
+        if(s == 0) cnt--;
+        
+        System.out.println(cnt);
     }
 
-    private static void search(int cur, int total) {
-
-        if(cur == N) {
-            if(total == S) result++;
+    private static void func(int cur, int tot) {
+        if(cur == n) {
+            if(tot == s) cnt++;
             return;
         }
-        search(cur + 1, total);
-        search(cur+1, total+arr[cur]);
+
+        func(cur+1, tot);
+        func(cur + 1, tot + arr[cur]);
     }
 }
