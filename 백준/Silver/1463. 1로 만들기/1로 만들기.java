fdec7ce27a811a0e4ebb6eb8bd4a6d503ff44ef4
Younghwan Hwang <dudghks5722@gmail.com>
2024-10-12 21:44:54 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18004 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,30 +1,25 @@
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Iterator;
+import java.io.*;
 
 public class Main {
-
-    static int[] dp = new int[1000005];
-    static int n;
+    
+    private static int n;
+    private static int[] d;
 
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        dp[1] = 0;
+        
+        n = Integer.parseInt(br.readLine());
+        d = new int[n+1];
+        d[1] = 0;
+        
         for(int i = 2; i <= n; i++) {
-
-            dp[i] = dp[i-1]+1;
-            if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-            if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            d[i] = d[i-1] + 1;
+            if(i%2 == 0) d[i] = Math.min(d[i], d[i/2] + 1);
+            if(i%3 == 0) d[i] = Math.min(d[i], d[i/3] + 1);
         }
-
-        System.out.println(dp[n]);
-
-
+        
+        System.out.println(d[n]);
     }
 }
