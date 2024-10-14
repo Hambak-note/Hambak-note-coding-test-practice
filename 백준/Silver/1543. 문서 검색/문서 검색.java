a71e052dae5ff6eb2562a9b95058af45cc1a17df
Younghwan Hwang <dudghks5722@gmail.com>
2024-10-14 20:53:01 +0900

    [Silver V] Title: 문서 검색, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,27 +1,26 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
     private static String inputStr;
-    private static String searchStr;
+    private static String targetStr;
+    private static int answer;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         inputStr = br.readLine();
-        searchStr = br.readLine();
+        targetStr = br.readLine();
+        answer = 0;
 
         int inputStrLen = inputStr.length();
-        int searchStrLen = searchStr.length();
+        int targetStrLen = targetStr.length();
 
-        String replacedInputStr = inputStr.replace(searchStr, "");
-        int replacedInputStrLen = replacedInputStr.length();
-        System.out.println((inputStrLen - replacedInputStrLen) / searchStrLen);
+        String replacedStr = inputStr.replace(targetStr, "");
+        int replacedStrLen = replacedStr.length();
 
+        answer = (inputStrLen - replacedStrLen) / targetStrLen;
+        System.out.println(answer);
     }
 }
